87a69423c8f7256ec14d3009d4513817b9e6bc68
Prem Sagar S <reallypremsagar@gmail.com>
2023-10-04 16:20:27 +0530

    Code simplification

--- PreComputedTables.py
+++ PreComputedTables.py
@@ -11,8 +11,7 @@ class PreComputedTables:
         self.kingAttackTable = [uint64(0)] * 64
 
         self.computeKnightAttackTable()
-        self.computePawnAttackTable()
-        self.computePawnPushTable()
+        self.computePawnTable()
         self.computeKingAttackTable()
 
         # number of squares to edge in 8 directions for all squares
@@ -25,8 +24,8 @@ class PreComputedTables:
         # Reference: https://www.chessprogramming.org/Magic_Bitboards
         self.bishopOccupancyMask = [uint64(0)] * 64
         self.rookOccupancyMask = [uint64(0)] * 64
-        self.computeBishopOccupancyMasks()
-        self.computeRookOccupancyMasks()
+        self.computeOccupancyMask(piece=BISHOP)
+        self.computeOccupancyMask(piece=ROOK)
 
         # Magic number generation. Can be generated if needed.
         # Generating magic numbers takes a long time so a list of valid magic numbers
@@ -53,26 +52,19 @@ class PreComputedTables:
             )
             self.knightAttackTable[i] = attacks
 
-    def computePawnAttackTable(self) -> None:
+    def computePawnTable(self) -> None:
         for i in range(8, 56):
             bitBoard = setBit(uint64(0), i)
             attacks = northeast(bitBoard) | northwest(bitBoard)
+            pushes = north(bitBoard)
+            self.pawnPushTable[WHITE][i] = pushes
             self.pawnAttackTable[WHITE][i] = attacks
 
         for i in range(8, 56):
             bitBoard = setBit(uint64(0), i)
             attacks = southeast(bitBoard) | southwest(bitBoard)
-            self.pawnAttackTable[BLACK][i] = attacks
-
-    def computePawnPushTable(self) -> None:
-        for i in range(8, 56):
-            bitBoard = setBit(uint64(0), i)
-            pushes = north(bitBoard)
-            self.pawnPushTable[WHITE][i] = pushes
-
-        for i in range(8, 56):
-            bitBoard = setBit(uint64(0), i)
             pushes = south(bitBoard)
+            self.pawnAttackTable[BLACK][i] = attacks
             self.pawnPushTable[BLACK][i] = pushes
 
     def computeKingAttackTable(self) -> None:
@@ -112,37 +104,27 @@ class PreComputedTables:
                     min(southMax, eastMax),
                 ]
 
-    def computeBishopOccupancyMasks(self) -> None:
+    def computeOccupancyMask(self, piece: int) -> None:
         for i in range(64):
             attacks = uint64(0)
-            for directionIndex in range(4, 8):
+            startIndex = 4 if piece == BISHOP else 0
+            endIndex = 8 if piece == BISHOP else 4
+            for directionIndex in range(startIndex, endIndex):
                 for n in range(1, self.numSquaresToEdge[i][directionIndex]):
                     endSquareIndex = i + DIRECTION_OFFSETS[directionIndex] * n
                     attacks = setBit(attacks, endSquareIndex)
-            self.bishopOccupancyMask[i] = attacks
+            if piece == BISHOP:
+                self.bishopOccupancyMask[i] = attacks
+            else:
+                self.rookOccupancyMask[i] = attacks
 
-    def computeRookOccupancyMasks(self) -> None:
-        for i in range(64):
-            attacks = uint64(0)
-            for directionIndex in range(0, 4):
-                for n in range(1, self.numSquaresToEdge[i][directionIndex]):
-                    endSquareIndex = i + DIRECTION_OFFSETS[directionIndex] * n
-                    attacks = setBit(attacks, endSquareIndex)
-            self.rookOccupancyMask[i] = attacks
-
-    def findBishopLegalMoveMask(self, startSquare: int, blockerbb: uint64) -> uint64:
+    def findLegalMoveMask(
+        self, startSquare: int, blockerbb: uint64, piece: int
+    ) -> uint64:
         legalMoves = uint64(0)
-        for directionIndex in range(4, 8):
-            for n in range(1, self.numSquaresToEdge[startSquare][directionIndex] + 1):
-                endSquareIndex = startSquare + DIRECTION_OFFSETS[directionIndex] * n
-                legalMoves = setBit(legalMoves, endSquareIndex)
-                if getBit(blockerbb, endSquareIndex) == (1):
-                    break
-        return legalMoves
-
-    def findRookLegalMoveMask(self, startSquare: int, blockerbb: uint64) -> uint64:
-        legalMoves = uint64(0)
-        for directionIndex in range(0, 4):
+        startIndex = 4 if piece == BISHOP else 0
+        endIndex = 8 if piece == BISHOP else 4
+        for directionIndex in range(startIndex, endIndex):
             for n in range(1, self.numSquaresToEdge[startSquare][directionIndex] + 1):
                 endSquareIndex = startSquare + DIRECTION_OFFSETS[directionIndex] * n
                 legalMoves = setBit(legalMoves, endSquareIndex)
@@ -170,7 +152,7 @@ class PreComputedTables:
 
         return occupancy
 
-    def bishopMagicFinder(self, sq: int) -> uint64:
+    def magicFinder(self, sq: int, piece: int) -> uint64:
         # Reference: https://www.chessprogramming.org/Looking_for_Magics
         mask = uint64(0)
         b = [uint64(0)] * 4096
@@ -178,13 +160,16 @@ class PreComputedTables:
         used = [uint64(0)] * 4096
         mask = uint64(0)
 
-        neededBits = self.bishopOccupancyMask[sq].bit_count()
-        mask = self.bishopOccupancyMask[sq]
+        mask = (
+            self.bishopOccupancyMask[sq]
+            if piece == BISHOP
+            else self.rookOccupancyMask[sq]
+        )
         n = mask.bit_count()
 
         for i in range(1 << n):
             b[i] = self.createBlockerBitboard(mask, i)
-            a[i] = self.findBishopLegalMoveMask(sq, b[i])
+            a[i] = self.findLegalMoveMask(sq, b[i], piece)
 
         for k in range(1000000):
             magic = random_uint64_fewbits()
@@ -195,7 +180,7 @@ class PreComputedTables:
             i = 0
             fail = 0
             while not fail and i < (1 << n):
-                j = int((b[i] * magic) >> uint64(64 - neededBits))
+                j = int((b[i] * magic) >> uint64(64 - n))
                 if used[j] == uint64(0):
                     used[j] = a[i]
                 elif used[j] != a[i]:
@@ -206,50 +191,10 @@ class PreComputedTables:
 
         raise Exception("Failed to find magic number")
 
-    def rookMagicFinder(self, sq: int) -> uint64:
-        # Reference: https://www.chessprogramming.org/Looking_for_Magics
-        mask = uint64(0)
-        b = [uint64(0)] * 4096
-        a = [uint64(0)] * 4096
-        used = [uint64(0)] * 4096
-        mask = uint64(0)
-
-        neededBits = self.rookOccupancyMask[sq].bit_count()
-        mask = self.rookOccupancyMask[sq]
-        n = mask.bit_count()
-
-        for i in range(1 << n):
-            b[i] = self.createBlockerBitboard(mask, i)
-            a[i] = self.findRookLegalMoveMask(sq, b[i])
-
-        for k in range(1000000):
-            magic = random_uint64_fewbits()
-            if (((mask * magic) & 0xFF00000000000000).bit_count()) < 6:
-                continue
-            for i in range(4096):
-                used[i] = uint64(0)
-            i = 0
-            fail = 0
-            while not fail and i < (1 << n):
-                j = int((b[i] * magic) >> uint64(64 - neededBits))
-                if used[j] == uint64(0):
-                    used[j] = a[i]
-                elif used[j] != a[i]:
-                    fail = 1
-                i += 1
-            if not fail:
-                return magic
-        print(self.rookMagicNumbers)
-        raise Exception("Failed to find magic number")
-
-    def computeBishopMagicNumbers(self) -> None:
+    def computeMagicNumbers(self) -> None:
         for square in range(64):
-            self.bishopMagicNumbers[square] = self.bishopMagicFinder(square)
-            print(f"Found for square number {square}")
-
-    def computeRookMagicNumbers(self) -> None:
-        for square in range(64):
-            self.rookMagicNumbers[square] = self.rookMagicFinder(square)
+            self.bishopMagicNumbers[square] = self.magicFinder(square, piece=BISHOP)
+            self.rookMagicNumbers[square] = self.magicFinder(square, piece=ROOK)
             print(f"Found for square number {square}")
 
     def computeBishopMagicBitboards(self) -> None:
@@ -264,7 +209,9 @@ class PreComputedTables:
 
             for i in range(numPatterns):
                 blockers[i] = self.createBlockerBitboard(occupancyMask, i)
-                legalMoveMasks[i] = self.findBishopLegalMoveMask(square, blockers[i])
+                legalMoveMasks[i] = self.findLegalMoveMask(
+                    square, blockers[i], piece=BISHOP
+                )
 
             for i in range(numPatterns):
                 j = int((blockers[i] * magicnumber) >> uint64(64 - n))
@@ -285,7 +232,9 @@ class PreComputedTables:
 
             for i in range(numPatterns):
                 blockers[i] = self.createBlockerBitboard(occupancyMask, i)
-                legalMoveMasks[i] = self.findRookLegalMoveMask(square, blockers[i])
+                legalMoveMasks[i] = self.findLegalMoveMask(
+                    square, blockers[i], piece=ROOK
+                )
 
             for i in range(numPatterns):
                 j = int((blockers[i] * magicnumber) >> uint64(64 - n))
